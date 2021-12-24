6e68752a390800fef6ddb8fa81a5ada8f113af3c
Vitali Semianiaka <jetframes@gmail.com>
2021-12-24 17:25:51 +0300

    first structured sources

---
+++ Processors/CmdCommand/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+var process = Process.Start(new ProcessStartInfo
+{
+    UseShellExecute = false,
+    FileName = args[0],
+    Arguments = args[1],
+});
+await process?.WaitForExitAsync();
+
+return process.ExitCode;
