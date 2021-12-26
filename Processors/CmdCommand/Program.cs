418ee02821919d29ce42d78e86072759a1435017
Vitali Semianiaka <jetframes@gmail.com>
2021-12-26 21:29:09 +0300

    fixed CmdProcessor

--- Processors/CmdCommand/Program.cs
+++ Processors/CmdCommand/Program.cs
@@ -1,11 +1,23 @@
-﻿using System.Diagnostics;
+﻿var process = new System.Diagnostics.Process();
 
-var process = Process.Start(new ProcessStartInfo
+process.StartInfo.UseShellExecute = false;
+process.StartInfo.FileName = args[0];
+foreach (var arg in args.Skip(1))
 {
-    UseShellExecute = false,
-    FileName = args[0],
-    Arguments = args[1],
-});
+    process.StartInfo.ArgumentList.Add(arg);
+}
+process.StartInfo.RedirectStandardError = true;
+process.StartInfo.RedirectStandardOutput = true;
+
+process.ErrorDataReceived += (sendingProcess, errorLine) => Console.Error.WriteLine(errorLine.Data);
+process.OutputDataReceived += (sendingProcess, dataLine) => Console.WriteLine(dataLine.Data);
+
+process.Start();
+
+process.BeginErrorReadLine();
+process.BeginOutputReadLine();
+
+
 await process?.WaitForExitAsync();
 
 return process?.ExitCode ?? default;
